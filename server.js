9b78e4d2411ca65afc935b9d3739caecb2b478dd
Glitch (hello-express) <none>
2020-06-04 19:16:19 +0000

    🍞👚 Checkpoint ./index.html:5152800/277:5660233/1052 ./jquery/jquery.js:5152800/186837 ./server.js:5660233/2 ./README.md:5660233/17

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.get("/jquery", (request, response) => {
   response.sendFile(__dirname + "/jquery/jquery.js");
 });
 
-
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/status", (request, response) => {
   response.sendStatus(200)
 });
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
