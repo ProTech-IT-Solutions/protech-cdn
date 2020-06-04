9769664ec7056468afecb4273325100acbc7e751
Glitch (hello-express) <none>
2020-06-04 18:41:20 +0000

    🍈🦁 Checkpoint ./package.json:5152800/1297 ./server.js:5152800/1274 ./index.html:5152800/62

--- server.js
+++ server.js
@@ -6,17 +6,16 @@
 const express = require("express");
 const app = express();
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
 
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/status", (request, response) => {
   response.sendStatus(200)
 });
-
-
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
